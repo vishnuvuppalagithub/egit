567288beba5a2552cd4ce8b77a1a99755749870c
vishnu <vishnu_20@yahoo.com>
2020-05-03 20:58:27 +0530

    init

--- javatest/src/javatest/filea.java
+++ javatest/src/javatest/filea.java
@@ -5,7 +5,7 @@ public class filea {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is first line");
+		System.out.println("This is first lines");
 
 	}
 
