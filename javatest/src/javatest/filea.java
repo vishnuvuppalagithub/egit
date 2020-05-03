36c78e1e28e4f3b8d851258b1d8192887de42770
Vishnu <test@yahoo.com>
2020-05-03 20:51:33 +0530

    Initial commit for project

---
+++ javatest/src/javatest/filea.java
@@ -0,0 +1,12 @@
+package javatest;
+
+public class filea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is first line");
+
+	}
+
+}
